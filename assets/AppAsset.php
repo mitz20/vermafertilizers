230e93e14e0a51bd4781e40dd6ece2a1c4034272
Amit Verma <mitz.lko18@gmail.com>
2017-06-01 15:24:35 +0530

    templates added

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,8 +19,19 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/custom.css',
+        'vendor/bootstrap/css/bootstrap.min.css',
+        'vendor/metisMenu/metisMenu.min.css',
+        'vendor/font-awesome/css/font-awesome.min.css',
+        'dist/css/sb-admin-2.css',
+        'dist/css/sb-admin-2.min.css',
     ];
     public $js = [
+        'js/custom.js',
+        'js/jquery.bxslider.min.js',
+        'vendor/bootstrap/js/bootstrap.min.js',
+        'vendor/metisMenu/metisMenu.min.js',
+        'dist/js/sb-admin-2.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- config/web.php
+++ config/web.php
@@ -19,7 +19,7 @@ $config = [
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'error/error',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
