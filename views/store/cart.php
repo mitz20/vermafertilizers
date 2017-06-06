f0f35ce4d03fec63df077a9e51ae5a00736e93c9
Amit Verma <mitz.lko18@gmail.com>
2017-06-06 11:10:54 +0530

    view change

--- views/store/cart.php
+++ views/store/cart.php
@@ -8,8 +8,15 @@ use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Cart';
-$this->title = (Yii::$app->session->get('cart')) ? ('Cart ( '. count(Yii::$app->session->get('cart')).' )') : ('Cart');
+$this->title = (Yii::$app->session->get('cart')) ? ('Cart ( ' . count(Yii::$app->session->get('cart')) . ' )') : ('Cart');
 $this->params['breadcrumbs'][] = $this->title;
+
+$colorArray = [
+    '0' => 'success',
+    '1' => 'info',
+    '2' => 'warning',
+    '3' => 'danger',
+];
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -24,5 +31,43 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     <?php endif; ?>
 
-    
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Context Classes
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Product Name</th>
+                                    <th>Price</th>
+                                    <th>Qty</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($products as $key => $product) { ?>
+                                    <tr class="<?php echo $colorArray[$key % 4]; ?>">
+                                        <td><?= $key+1 ?></td>
+                                        <td><?= $product->name ?></td>
+                                        <td><?= $product->price_per_unit ?></td>
+                                        <td>1</td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-6 -->
+    </div>
+    <!-- /.row -->
 </div>
